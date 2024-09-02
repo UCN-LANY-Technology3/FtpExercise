0a4ca97105d99dff01d2ed609f476e82445022b6
LANY <LANY@HYBRIDUCN-SQU1G>
2024-09-02 12:53:44 +0200

    Add project files.

---
+++ FtpExercise/FtpClient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FtpExercise
+{
+    internal class FtpClient
+    {
+        public List<string> GetFileList()
+        {
+            // Get a list of the files on the server and return a list of filenames.
+
+            throw new NotImplementedException();
+        }
+
+        public void UploadFile(string path)
+        {
+            // Upload a file from your hard drive to the server.
+
+            throw new NotImplementedException();
+        }
+
+        public void DownloadFile(string path)
+        {
+            // Download a file from the server and write it to your hard drive.
+
+            throw new NotImplementedException();
+        }
+    }
+}
